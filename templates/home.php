1870ee60c7a5b1ce3cd6f482c6515c460634a466
NabylBadid <badid.nabyl@yahoo.fr>
2020-04-25 16:37:36 +0200

    Mise en place du model avec les classes Article et Comment pour utiliser au maximum la POO

--- templates/home.php
+++ templates/home.php
@@ -1,9 +1,14 @@
 <?php
-
-use App\src\DAO\ArticleDAO;
-
 ?>
 <!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Mon blog</title>
+</head>
+
+<body>
+<div><!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="utf-8">
@@ -15,8 +20,29 @@ use App\src\DAO\ArticleDAO;
     <h1>Mon blog</h1>
     <p>En construction</p>
     <?php
+    foreach ($articles as $article)
+    {
+        ?>
+        <div>
+            <h2><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getId());?>"><?= htmlspecialchars($article->getTitle());?></a></h2>
+            <p><?= htmlspecialchars($article->getContent());?></p>
+            <p><?= htmlspecialchars($article->getAuthor());?></p>
+            <p>Créé le : <?= htmlspecialchars($article->getCreatedAt());?></p>
+        </div>
+        <br>
+        <?php
+    }
+    ?>
+</div>
+</body>
+</html>
+    <h1>Mon blog</h1>
+    <p>En construction</p>
+    <?php
     while($article = $articles->fetch())
     {
+        var_dump($article);
+
         ?>
         <div>
         <h2><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->id);?>"><?= htmlspecialchars($article->title);?></a></h2>
