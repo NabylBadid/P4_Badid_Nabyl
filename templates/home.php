f4c3b160222b6dedb08a5a5abfe0b9641d0357e1
NabylBadid <badid.nabyl@yahoo.fr>
2020-04-27 15:54:37 +0200

    Mise en place du systeme dinscription

--- templates/home.php
+++ templates/home.php
@@ -7,7 +7,10 @@
 <?= $this->session->show('delete_article'); ?>
 <?= $this->session->show('add_comment'); ?>
 <?= $this->session->show('flag_comment'); ?>
-<?= $this->session->show('delete_comment'); ?><br />
+<?= $this->session->show('delete_comment'); ?>
+<?= $this->session->show('register'); ?><br />
+<a href="../public/index.php?route=register">Inscription</a>
+<a href="../public/index.php?route=login">Connexion</a>
 <a href="../public/index.php?route=addArticle">Nouvel article</a>
 <?php
 foreach ($articles as $article)
